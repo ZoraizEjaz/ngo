d48c74752983d03d219df5ee640598a37527372b
zoraizBitBucket <zoraiz93@gmail.com>
2020-10-22 12:09:28 +0500

    fonts added

---
+++ ngo/lib/screens/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget{
+
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
